eb4697c605524b21d3a1ca75a1279f53b611ab61
Erik McComb <erik@erikmccomb.com>
2017-09-07 14:50:11 -0600

    initial commit

---
+++ src/components/FlashCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class flashCard extends React.Component{
+  render()
+  //front value and back value, 
+}
+
+export default flashCard;
---
+++ src/components/FlashList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class flashList extends React.Component{
+ //render list of cards input by user - list of objects - loop
+}
+
+export default flashList;
